a03698ea2e55a6e2029b574ad3af1021ea7a2f12
kowalix007 L.K <kowalix007@gmail.com>
2020-07-02 11:02:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,7 @@
 # Make your shoe class here!
 class Shoe 
-  attr_reader:brand, :color, :size, :material
+  attr_accessor:color
+  attr_reader:brand,:color,:size, :material
  def initialize (brand)
    @brand = brand
  end 
